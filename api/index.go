afe5b105c7245ac5332cce7cb111dd4ce4f9f3c1
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-14 12:39:11 +0200

    Update index.go

--- api/index.go
+++ api/index.go
@@ -10,7 +10,7 @@ const index = "" +
 <html>
 	<h1>sin(-x)*pow(1.5,-r): <a href="plot?expr=sin(-x)*pow(1.5,-r)">Try it!</a></h1>
 	<h1>sin(x*y/10)/10: <a href="plot?expr=sin(x*y/10)/10">Try it!</a></h1>
-	<h1>pow(2,sin(y))*pow(2,sin(x)): <a href="plot?expr=pow(2,sin(y))*pow(2,sin(x))">Try it!</a></h1>
+	<h1>pow(2,sin(y))*pow(2,sin(x))/12: <a href="plot?expr=pow(2,sin(y))*pow(2,sin(x))/12">Try it!</a></h1>
 </html>
 `
 
