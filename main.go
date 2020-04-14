a1fea59e254585319a40cd1f490cfdc4d1d79bc0
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-14 10:59:34 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/mbraeutig/go-plot/api"
+)
+
+func main() {
+	http.HandleFunc("/", api.Index)
+	http.HandleFunc("/plot", api.Plot)
+	http.HandleFunc("/health", api.Health)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
